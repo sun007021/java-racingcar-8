0a93e13d94c544743f61628cd5a80d74d7a9e252
sun <sun007021@gmail.com>
2025-10-27 23:56:12 +0900

    feat: integrate application

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,12 +1,53 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
+import racingcar.domain.RacingGame;
+import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class RacingGameController {
 
-    public RacingGameController() {
-        // TODO: 구현 예정
+    public void run() {
+        List<String> carNames = readAndValidateCarNames();
+        int roundCount = readAndValidateRoundCount();
+
+        RacingGame game = new RacingGame(carNames, roundCount);
+
+        OutputView.printResultMessage();
+        playGameWithOutput(game, roundCount);
+
+        List<String> winners = game.getWinners();
+        OutputView.printWinners(winners);
     }
 
-    public void run() {
-        // TODO: 구현 예정
+    private List<String> readAndValidateCarNames() {
+        List<String> carNames = InputView.readCarNames();
+        String input = String.join(",", carNames);
+        InputValidator.validateCarNames(input);
+        return carNames;
+    }
+
+    private int readAndValidateRoundCount() {
+        int roundCount = InputView.readRoundCount();
+        InputValidator.validateRoundCount(String.valueOf(roundCount));
+        return roundCount;
+    }
+
+    private void playGameWithOutput(RacingGame game, int roundCount) {
+        for (int i = 0; i < roundCount; i++) {
+            game.playRound();
+            printRoundResult(game);
+        }
+    }
+
+    private void printRoundResult(RacingGame game) {
+        List<Car> cars = game.getCars();
+        for (Car car : cars) {
+            OutputView.printCarStatus(car.getName(), car.getPosition());
+        }
+        OutputView.printRoundResult();
     }
 }
