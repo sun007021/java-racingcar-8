61e8be3dc6dcee9e2fd8a34af5aac0417110c562
sun <sun007021@gmail.com>
2025-10-27 23:30:00 +0900

    feat(OutputView): add feature to print each round result and final winner car

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,17 +3,39 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_PREFIX = "최종 우승자 : ";
+    private static final String POSITION_SYMBOL = "-";
+    private static final String CAR_STATUS_FORMAT = "%s : %s";
+    private static final String WINNER_DELIMITER = ", ";
 
     private OutputView() {
     }
 
+    public static void printResultMessage() {
+        System.out.println();
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public static void printCarStatus(String carName, int position) {
+        System.out.println(formatCarStatus(carName, position));
+    }
+
     public static String formatCarStatus(String carName, int position) {
-        // TODO: 구현 예정
-        return null;
+        String positionDisplay = POSITION_SYMBOL.repeat(position);
+        return String.format(CAR_STATUS_FORMAT, carName, positionDisplay);
+    }
+
+    public static void printRoundResult() {
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println();
+        System.out.println(formatWinners(winners));
     }
 
     public static String formatWinners(List<String> winners) {
-        // TODO: 구현 예정
-        return null;
+        return WINNER_PREFIX + String.join(WINNER_DELIMITER, winners);
     }
 }
